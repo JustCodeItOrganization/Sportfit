e9cf517bcc1c16c9ca0bb391f24cb42d337aa58c
kereminci <kereminci52@gmail.com>
2022-12-06 20:52:40 +0300

    appbar title theme changed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_2/view/home_page_view.dart';
 import 'package:flutter_application_2/view_model/home_view_model.dart';
 import 'package:flutter_application_2/view_model/input_view_model.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -20,10 +21,11 @@ class MyApp extends StatelessWidget {
         title: 'MVVM',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          primarySwatch: Colors.blue,
-          brightness: Brightness.light,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
+            primarySwatch: Colors.blue,
+            brightness: Brightness.light,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            appBarTheme:
+                AppBarTheme(titleTextStyle: GoogleFonts.poppins(fontSize: 26))),
         //home: ProfileView(),
         //home: HomeView(),
         home: PageHomeView(),
