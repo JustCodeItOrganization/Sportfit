3edf286d92737ce74c6306f5ac342c51a8651216
Halil Uzer <haliluzer@outlook.com>
2022-12-11 01:11:28 +0300

    added file i/o

--- lib/view/profile_view.dart
+++ lib/view/profile_view.dart
@@ -1,7 +1,39 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:accordion/accordion.dart';
 import 'package:flutter_application_2/view/input_module_view.dart';
 import 'package:flutter_application_2/widgets/dropdown_button.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:convert';
+
+class Profile {
+  final String weight;
+  final String height;
+
+  Profile(this.weight, this.height);
+
+  Profile.fromJson(Map<String, dynamic> json): weight = json['weight'], height = json['height'];
+
+  Map<String, dynamic> toJson() => {
+    'weight' : weight,
+    'height' : height,
+  };
+}
+
+class ProfileStorage{
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/data.json');
+  }
+
+}
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
