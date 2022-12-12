5069bf745a7b68996352b91255d945ae6eee8f56
eren-elma <erennelma@gmail.com>
2022-12-12 21:00:29 +0300

    calori functions

--- lib/view_model/utility_functions_view_model.dart
+++ lib/view_model/utility_functions_view_model.dart
@@ -39,22 +39,22 @@ double rmr_calculation(bool gender, int age, double weight, double height){
   return initCost + weight* weightMultiplier + height * heightMultiplier - age * ageMultiplier;
 }
 
-double activity_level(int activity){
+double activity(int fitnesslevel){
   // 1 -> sedentary (little to no exercise)
   // 2 -> lightly active (light exercise 1–3 days per week)
   // 3 -> moderately active (moderate exercise 3–5 days per week)
   // 4 -> very active (hard exercise 6–7 days per week)
   // 5 -> extra active (very hard exercise, training, or a physical job)
-  if (activity == 1){
+  if (fitnesslevel == 1){
     return 1.2;
   }
-  else if (activity == 2){
+  else if (fitnesslevel == 2){
     return 1.375;
   }
-  else if (activity == 3){
+  else if (fitnesslevel == 3){
     return 1.55;
   }
-  else if (activity == 4){
+  else if (fitnesslevel == 4){
     return 1.725;
   }
   else {
@@ -62,8 +62,37 @@ double activity_level(int activity){
   }
 }
 
-double maintain_weight(bool gender, int age, double weight, double height, int activity){
+double maintain_weight(bool gender, int age, double weight, double height, int fitnesslevel){
   double rmr = rmr_calculation(gender = gender, age = age, weight= weight , height = height);
-  double activityMultiplier = activity_level(activity);
+  double activityMultiplier = activity(fitnesslevel);
   return rmr * activityMultiplier; 
 }
+
+double food_calorie(double base_weight, double base_calorie, double weight){
+  return weight/base_weight * base_calorie;
+}
+
+double workout_calorie(bool gender, double weight, double time){
+  //gender == true -> man.
+  double genderRatio = 1.0;
+  double baseWeight = 40;
+  double upperWeight = 150;
+  double baseCalorie = 1.7;
+  double upperCalorie = 3.1;
+  double weightRatio;
+  double calorieRatio;
+  if (weight >= baseWeight && weight <= upperWeight){
+    weightRatio = (weight - baseWeight)/(upperWeight - baseWeight);
+  }
+  else if (weight > upperWeight){
+    weightRatio = 1.0;
+  }
+  else {
+    weightRatio = 0.0;
+  }
+  if (!gender)genderRatio = 0.6074;
+  calorieRatio = baseCalorie + (upperCalorie - baseCalorie) * weightRatio;
+  return time * calorieRatio * genderRatio;
+   
+}
+  
