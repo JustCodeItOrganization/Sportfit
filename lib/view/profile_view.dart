5cfc215eaee2400456b043939dd6bdfb850bce85
Halil Uzer <haliluzer@outlook.com>
2022-12-18 22:54:54 +0300

    new vars added

--- lib/view/profile_view.dart
+++ lib/view/profile_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:accordion/accordion.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_2/view/input_module_view.dart';
 import 'package:flutter_application_2/widgets/dropdown_button.dart';
 import 'package:flutter/cupertino.dart';
 import './profile_storage.dart';
+import './../widgets/styled_text.dart';
 
 
 
@@ -18,14 +20,19 @@ class ProfileView extends StatefulWidget{
 class _ProfileViewState extends State<ProfileView> {
   int weight = 0;
   int height = 0;
+  int age = 0;
+  int fitnessLevel = 0;
+  String gender = 'Erkek';
   final _weightTextController = TextEditingController();
   final _heightTextController = TextEditingController();
   final _ageTextController = TextEditingController();
   void getProfileFromStorage() async{
-    Profile profile = await Profile().readProfileFromStorage();
+    Profile profile = await Profile.readProfileFromStorage();
     setState(() {
       weight = profile.weight;
       height = profile.height;
+      age = profile.age;
+      fitnessLevel = profile.fitnessLevel;
     });
   }
 
@@ -37,11 +44,16 @@ class _ProfileViewState extends State<ProfileView> {
   Widget build(BuildContext context) {
     return Column(
         children: [
-          Row(children: [Text(weight.toString()), Text(height.toString())]),
+          Column(
+              children: [
+                Row(children: [
+                  StyledText('Ağırlık:${weight.toString()}'),
+                  StyledText('Uzunluk:${height.toString()}')])]),
+          Row(children: [StyledText('Cinsiyet:${gender.toString()}'), StyledText('Yaş:${age.toString()}')],),
           Accordion(
             children: [
               AccordionSection(
-                header:const Text('Vücut  özellikleri'),
+                header:const Text('Vücut özellikleri'),
                 content: Column(
                   children: [
                     TextField(
@@ -49,18 +61,29 @@ class _ProfileViewState extends State<ProfileView> {
                       decoration:const InputDecoration(
                         hintText: 'Ağırlık(kg)',
                       )
-
                     ),
                     TextField(controller: _heightTextController,decoration: InputDecoration(hintText: 'Uzunluk(cm)')),
                     TextField(controller: _ageTextController,decoration: InputDecoration(hintText: 'Yaş')),
                     DropDownMenu(
                         items: dropDownMenuItems,
-                        onChangedCallBack: (String) => {},
-                        currentItem: "Male"),
+                        onChangedCallBack: (selected) => {setState(() {
+                          String value;
+                          if(selected == null){
+                            value = 'Erkek';
+                          }
+                          else{
+                            value = selected;
+                          }
+                              gender = value;
+                        })},
+                        currentItem: "Erkek",
+
+                    ),
                     ElevatedButton(onPressed: (){
                       setState(() {
                         weight = int.parse(_weightTextController.text);
                         height = int.parse(_heightTextController.text);
+                        age = int.parse(_ageTextController.text);
                       });
                     }, child: Text('Verileri kaydet'))
                   ],
@@ -95,7 +118,7 @@ class _ProfileViewState extends State<ProfileView> {
                 ),
               ),
               AccordionSection(
-                header: Text('Fitness Level Hesaplayıcı'),
+                header: Text('Fitness Leveli'),
                 content: Column(
                   children: [
                     const InputTextField(
@@ -113,12 +136,8 @@ class _ProfileViewState extends State<ProfileView> {
               ),
             ],
           ),
-          InputTextField(
-            labelText: 'Fitness Level Output',
-            isEnabled: false,
-          ),
           ],
       );
   }
-  List<String> get dropDownMenuItems => ["Male", "Female"];
+  List<String> get dropDownMenuItems => ["Erkek", "Kadın"];
 }
