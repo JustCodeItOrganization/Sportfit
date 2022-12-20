0dd0ea199db7634654ce8533f694c114f5736e6b
kereminci <kereminci52@gmail.com>
2022-12-20 14:43:01 +0300

    added model classes and table queries

--- lib/core/init/database/database_manager.dart
+++ lib/core/init/database/database_manager.dart
@@ -8,19 +8,31 @@ class DatabaseManager {
     if (_db == null) {
       print("init()");
       var databasesPath = await getDatabasesPath();
-      String path = join(databasesPath, 'demo.db');
+      String path = join(databasesPath, 'demo5.db');
 
       _db = await openDatabase(path, version: 1,
           onCreate: (_db, int version) async {
         // olusturmak istenilen table lar burda olustururlur
+
+        // dinamik tablolar
         await _db.execute(
-            'CREATE TABLE Test (id INTEGER PRIMARY KEY, name TEXT, value INTEGER, num REAL)');
+            "CREATE TABLE Profile (age INTEGER,gender BOOLEAN ,height REAL,weight REAL,fitnessLevel INTEGER,calorieGoal INTEGER)");
+        await _db.execute(
+            'CREATE TABLE personal_exercise (exercise_id INTEGER PRIMARY KEY ,time INTEGER)');
+        await _db.execute(
+            'CREATE TABLE kişisel_yemek (yemek_id INTEGER PRIMARY KEY,initialAmount DOUBLE,currentAmount DOUBLE)');
+        // statik tablolar
+        await _db.execute(
+            "CREATE TABLE Exercise (id INTEGER PRIMARY KEY,name TEXT,description TEXT,met REAL ,time INTEGER ,intensity INTEGER,is_completed BOOLEAN,set_count INTEGER,video_url TEXT,image_path TEXT)");
+        await _db.execute(
+            "CREATE TABLE Food (id INTEGER,name TEXT,baseCalories INTEGER,protein REAL,carbohydrate REAL,fat REAL,PRIMARY KEY (id))");
       });
     }
   }
 
   Future<void> insert(String table, Map<String, Object?> values) async {
     if (_db != null) {
+      print(values);
       await _db!
           .insert(table, values, conflictAlgorithm: ConflictAlgorithm.replace);
     }
