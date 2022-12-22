2c1d58d9f28cdac5efc50886b98fbfc41bdead78
Mücahit Ertano <mucahitertano.me@gmail.com>
2022-12-23 00:42:21 +0300

    modified

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,8 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import path_provider_macos
-import url_launcher_macos
 import sqflite
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
